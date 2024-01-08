b2516feaf6edee50d7ae0ebe4ee3acf40a131698
이지희 <jeehee2610@gmail.com>
2024-01-08 21:17:06 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,7 +1,7 @@
 function solution(array) {
     array = array.sort((a,b) => a-b);
     
-    const middleNum = Math.floor(array.length/2) ;
+    const middleNum = Math.floor(array.length/2);
 
     const answer = array[middleNum];
     return answer;
