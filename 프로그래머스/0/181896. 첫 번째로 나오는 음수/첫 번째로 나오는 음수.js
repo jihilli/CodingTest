2e62b61305d5efee382ed1aca6ca23e1e1eaf4d6
이지희 <jeehee2610@gmail.com>
2024-01-26 01:53:07 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    var answer ;
+    
+    for(let i = 0; i < num_list.length; i++){
+        if(num_list[i] < 0) {
+            answer = i;
+              break;}
+    }
+
+       return (answer === undefined) ? -1 : answer;          
+    
+}
